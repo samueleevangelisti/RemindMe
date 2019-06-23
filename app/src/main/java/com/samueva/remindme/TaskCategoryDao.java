1bc52453c3ef69a588e398dde4da992ee68a0e3a
Samuele Evangelisti <samueleevangelisti@yahoo.it>
2019-06-23 23:18:23 +0200

    Modificata la strategia per gestire il conflitto di inserimenti della stessa chiave primaria nelle category. Ora se si tenta di inserire più volte la stessa chiave primaria (viene sempre fatto all'avvio dell'app) il comando viene ignorato e tutto continua f funzionare

--- app/src/main/java/com/samueva/remindme/TaskCategoryDao.java
+++ app/src/main/java/com/samueva/remindme/TaskCategoryDao.java
@@ -3,6 +3,7 @@ package com.samueva.remindme;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface TaskCategoryDao {
     @Query("SELECT * FROM taskcategory")
     List<TaskCategory> getAll();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertAll(TaskCategory... taskCategories);
 
     @Delete
